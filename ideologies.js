cf2b1f3278b9be0351558ba0a81c41bec5035cb9
Quark <72112478+QUARK88@users.noreply.github.com>
2021-04-03 19:24:04 -0300

    Add files via upload

---
+++ ideologies.js
@@ -0,0 +1,26 @@
+ideologies = [
+    {
+        "name": "Centrism",
+        "stats": {
+            "biden": 50,
+            "binte": 50,
+            "bstru": 50,
+            "bimpl": 50,
+            "bappl": 50,
+            "bcult": 50,
+            "bmigr": 50,
+        }
+    },
+    {
+        "name": "Quarkism",
+        "stats": {
+            "biden": 90,
+            "binte": 0,
+            "bstru": 0,
+            "bimpl": 0,
+            "bappl": 80,
+            "bcult": 20,
+            "bmigr": 75,
+        }
+    }
+];
